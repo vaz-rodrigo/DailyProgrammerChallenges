e95b9eb9a4af92fe1b59b453b4f4a6441e0c3286
Freddie Vargus <fv4.programmer@gmail.com>
2016-03-11 23:52:52 -0500

    Add Twilio sms to notify owner of updated repo

--- send-data.sh
+++ send-data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 git add -A; git commit -m "Add this weeks challenges"; git push
+
+read -r line < ../mynumber.txt
+
+echo "Repo Successfully Updated" | .././twilio-sms.sh -c /.twiliorc/config $line
