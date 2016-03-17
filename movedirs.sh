5ba9e6a78f9d3ba6675fcba236c80ef237d7552f
Freddie Vargus <fv4.programmer@gmail.com>
2016-03-17 15:06:10 -0400

    Remove reserved chars from filenames

--- movedirs.sh
+++ movedirs.sh
@@ -3,8 +3,10 @@
 # Author: Freddie Vargus (github.com/FreddieV4)
 # Purpose: Used to move challenge directories into parent directories
 
-find -maxdepth 1 -type d -iname "*Easy*" -exec mv {} "Easy Challenges"/ \;
+find -maxdepth 1 -type d -iname "*[Easy*" -exec mv {} "Easy Challenges"/ \;
 
-find -maxdepth 1 -type d -iname "*Intermediate*" -exec mv {} "Intermediate Challenges"/ \;
+find -maxdepth 1 -type d -iname "*[Intermediate*" -exec mv {} "Intermediate Challenges"/ \;
 
-find -maxdepth 1 -type d -iname "*Hard*" -exec mv {} "Hard Challenges"/ \;
+find -maxdepth 1 -type d -iname "*[Difficult*" -exec mv {} "Hard Challenges"/ \;
+
+find -maxdepth 1 -type d -iname "*[Hard*" -exec mv {} "Hard Challenges"/ \;
